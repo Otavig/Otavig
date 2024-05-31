9a36e2fef9737bfe16bd612e158e9e26516635a3
Otávio <125999228+Otavig@users.noreply.github.com>
2024-05-31 20:47:30 -0300

    Add files via upload

---
+++ public/js/form.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('formulario');
+  
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+  
+      const formData = new FormData(form);
+      const phoneValue = formData.get('phone_form').replace(/\D/g, ''); // Remover caracteres não numéricos
+      const celular = phoneValue ? parseInt(phoneValue) : null; // Converter para número ou null se for vazio
+      const data = {
+        nome: formData.get('name_form'),
+        email: formData.get('email_form'),
+        celular: celular,
+        msg: formData.get('msg_form')
+      };
+  
+      if (data.nome.length > 255){
+        alert('Invalido. Máximo de 255 caracters!');
+        return;
+      } else if (data.msg.length > 200){
+        alert('Invalido. Máximo de 200 caracters!');
+        return;
+      } else if (data.email.length > 256){
+        alert('Invalido. Máximo de 256 caracters!')
+        return;
+      } else if (!data.celular || data.celular.toString().length > 15 || data.celular.toString().length < 13){
+        alert('Invalido. Máximo e Minimo de 15 números!')
+        return;
+      }
+  
+      fetch('http://localhost:3000/enviar-formulario', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Resposta do servidor:', data);
+        // Limpar os campos do formulário após o envio bem-sucedido
+        document.getElementById('name_form').value = "";
+        document.getElementById('email_form').value = "";
+        document.getElementById('phone_form').value = "";
+        document.getElementById('msg_form').value = "";
+      })
+      .catch(error => {
+        console.error('Erro ao enviar formulário:', error);
+      });
+    });
+  });
+  
